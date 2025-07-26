1a62641305e8f7c0522758f93c82a25adf238cee
Higor Jardini <contac@higorjardini.dev>
2025-07-26 01:04:39 -0300

    Adicionar o Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,47 @@
 import Fastify from "fastify";
+import swagger from "@fastify/swagger";
+import swaggerUI from "@fastify/swagger-ui";
 import { getEnv } from "@config/env";
 import { registerRoutes } from "@config/routes";
 
 async function bootstrap() {
   const app = Fastify({ logger: true });
 
-  const { PORT } = getEnv();
+  await app.register(swagger, {
+    openapi: {
+      info: {
+        title: "API Projeto Teste LuizaLabs",
+        description: "Documentação gerada pelo Swagger",
+        version: "1.0.0",
+      },
+      servers: [
+        {
+          url: `http://localhost:${getEnv().PORT}`,
+          description: "Local server",
+        },
+      ],
+    },
+  });
+
+  await app.register(swaggerUI, {
+    routePrefix: "/docs",
+    uiConfig: {
+      docExpansion: "list",
+      deepLinking: false,
+    },
+    staticCSP: true,
+  });
 
   await registerRoutes(app);
 
+  await app.ready();
+  await app.swagger();
+
+  const { PORT } = getEnv();
   await app.listen({ port: PORT });
+
   console.log(`Server rodando na porta ${PORT}`);
+  console.log(`Swagger disponível em http://localhost:${PORT}/docs`);
 }
 
 bootstrap();
