c9d58d2e0365a27d4f30d83a75ce55341b45686f
Higor Jardini <contac@higorjardini.dev>
2025-07-28 00:26:25 -0300

    Adicionar diagramas de entidade-relacionamento e de fluxo, além de atualizar o esquema SQL com novas tabelas e relacionamentos

--- init.sql
+++ init.sql
@@ -8,6 +8,35 @@ CREATE TABLE UserLogins (
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+CREATE TABLE Users (
+    user_id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Uploads (
+    upload_id SERIAL PRIMARY KEY,
+    login_id BIGINT NOT NULL,
+    uploaded_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (login_id) REFERENCES UserLogins(login_id)
+);
+
+CREATE TABLE Orders (
+    order_id SERIAL PRIMARY KEY,
+    purchase_date DATE NOT NULL,
+    total DECIMAL(10, 2) NOT NULL,
+    user_id INT NOT NULL,
+    upload_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES Users(user_id),
+    FOREIGN KEY (upload_id) REFERENCES Uploads(upload_id)
+);
+
+CREATE TABLE Products (
+    product_id SERIAL PRIMARY KEY,
+    value DECIMAL(10, 2) NOT NULL,
+    order_id INT NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id)
+);
+
 -- Trigger para atualizar automaticamente o updated_at
 CREATE OR REPLACE FUNCTION update_updated_at_column()
 RETURNS TRIGGER AS $$
@@ -21,3 +50,9 @@ CREATE TRIGGER update_userlogins_updated_at
 BEFORE UPDATE ON UserLogins
 FOR EACH ROW
 EXECUTE FUNCTION update_updated_at_column();
+
+-- Índices
+CREATE INDEX idx_uploads_login_id ON Uploads(login_id);
+CREATE INDEX idx_orders_user_id ON Orders(user_id);
+CREATE INDEX idx_orders_upload_id ON Orders(upload_id);
+CREATE INDEX idx_products_order_id ON Products(order_id);
